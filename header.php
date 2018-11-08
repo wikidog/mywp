2a4dadae0cb706002b3588edc7c70e478452d1db
WiKiDog W <16198436+wikidog@users.noreply.github.com>
2018-11-08 15:00:01 -0800

    20181108-01

--- header.php
+++ header.php
@@ -2,7 +2,9 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<?php wp_head(); ?>
 </head>
--- page.php
+++ page.php
@@ -16,20 +16,50 @@
 
   <div class="container container--narrow page-section">
 
-<?php $theParent = wp_get_post_parent_id(get_the_id()); ?>
-<?php if ($theParent) : ?>
+<?php
+  // only display the following section if the page has parent page
+  //
+  $theParent = wp_get_post_parent_id(get_the_id());
+
+  if ($theParent) {
+?>
     <div class="metabox metabox--position-up metabox--with-home-link">
       <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
     </div>
-<?php endif; ?>
+<?php
+  }
+?>
 
+<?php
+  // only display the following section
+  //   if the page has parent page or has child page(s)
+  //
+  $hasChildren = get_pages(array(
+    'child_of' => get_the_ID(),
+  ));
+
+  if ($theParent || $hasChildren) {
+?>
     <div class="page-links">
-      <h2 class="page-links__title"><a href="<?php echo get_the_permalink(get_the_id()) ?>"><?php the_title(); ?></a></h2>
+      <h2 class="page-links__title"><a href="<?php echo get_the_permalink(get_the_id($theParent)) ?>"><?php echo get_the_title($theParent); ?></a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+        <?php
+          if ($theParent) {
+            $findChildrenOf = $theParent;
+          } else {
+            $findChildrenOf = get_the_id();
+          }
+          wp_list_pages(array(
+            'title_li' => NULL,
+            'child_of' => $findChildrenOf,
+            'sort_column' => 'menu_order',
+          ));
+        ?>
       </ul>
     </div>
+<?php
+  }
+?>
 
     <div class="generic-content">
     <?php the_content(); ?>
