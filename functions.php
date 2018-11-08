b9f0a843139a72bd6c635284b89fd7043131ca36
WiKiDog H <16198436+wikidog@users.noreply.github.com>
2018-11-07 20:41:20 -0800

    20181107-02

--- functions.php
+++ functions.php
@@ -12,9 +12,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-function my_theme_enqueue_styles() {
 
-  wp_enqueue_script('my-theme-js', get_stylesheet_directory_uri() . '/js/scripts-bundled.js', NULL, '1.0', true);
+/**
+ * Theme Setup
+ */
+function my_theme_setup() {
+  // enables plugins and themes to manage the document <title> tag
+  add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'my_theme_setup');
+
+/**
+ * Load front-end CSS styles
+ */
+function my_theme_css() {
 
   wp_enqueue_style( 'my-google-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 
@@ -27,4 +39,16 @@ function my_theme_enqueue_styles() {
       wp_get_theme()->get('Version')
   );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+add_action( 'wp_enqueue_scripts', 'my_theme_css' );
+
+/**
+ * Load front-end js
+ */
+function my_theme_js() {
+
+  wp_enqueue_script('my-theme-js', get_stylesheet_directory_uri() . '/js/scripts-bundled.js', NULL, '1.0', true);
+
+}
+
+add_action( 'wp_enqueue_scripts', 'my_theme_js' );
--- page.php
+++ page.php
@@ -1,24 +1,30 @@
 
 <?php get_header(); ?>
 
+<!-- Start the Loop. -->
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title">Our History</h1>
+      <h1 class="page-banner__title"><?php the_title(); ?></h1>
       <div class="page-banner__intro">
-        <p>Learn how the school of your dreams got started.</p>
+        <p>FIX ME!!!</p>
       </div>
     </div>
   </div>
 
   <div class="container container--narrow page-section">
 
+<?php $theParent = wp_get_post_parent_id(get_the_id()); ?>
+<?php if ($theParent) : ?>
     <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
     </div>
+<?php endif; ?>
 
     <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <h2 class="page-links__title"><a href="<?php echo get_the_permalink(get_the_id()) ?>"><?php the_title(); ?></a></h2>
       <ul class="min-list">
         <li class="current_page_item"><a href="#">Our History</a></li>
         <li><a href="#">Our Goals</a></li>
@@ -26,14 +32,11 @@
     </div>
 
     <div class="generic-content">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+    <?php the_content(); ?>
     </div>
 
   </div>
 
-<!-- Start the Loop. -->
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
   <!-- Display the Title as a link to the Post's permalink. -->
   <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
