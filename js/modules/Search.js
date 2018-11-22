cfe7742a28744ae376b5c219506f6a0cc1bd4239
WiKiDog H <16198436+wikidog@users.noreply.github.com>
2018-11-21 17:49:05 -0800

    20181121-02

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -6,19 +6,44 @@ class Search {
     this.closeButton = $('.search-overlay__close');
     this.searchOverlay = $('.search-overlay');
     this.events();
+    this.isOverlayOpen = false;
   }
 
   events() {
     this.openButton.on('click', this.openOverlay.bind(this));
     this.closeButton.on('click', this.closeOverlay.bind(this));
+
+    $(document).on('keydown', this.keyPressDispatcher.bind(this));
+  }
+
+  keyPressDispatcher(e) {
+    /**
+     *! since we are using 'keydown' event and if the user presses and holds
+     *! the key, the 'keydown' event will keep firing.
+     *! Using a flag (isOverlayOpen) is to avoid the handler function
+     *! gets called repeatedly.
+     */
+    if (e.keyCode === 83 && !this.isOverlayOpen) {
+      // this is the "s" key
+      this.openOverlay();
+    }
+
+    if (e.keyCode === 27 && this.isOverlayOpen) {
+      // this is the "esp" key
+      this.closeOverlay();
+    }
   }
 
   openOverlay() {
     this.searchOverlay.addClass('search-overlay--active');
+    $('body').addClass('body-no-scroll');
+    this.isOverlayOpen = true;
   }
 
   closeOverlay() {
     this.searchOverlay.removeClass('search-overlay--active');
+    $('body').removeClass('body-no-scroll');
+    this.isOverlayOpen = false;
   }
 }
 
