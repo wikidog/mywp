58020d72a8ee8afb05e816a07b66ebaea043f87d
WiKiDog W <16198436+wikidog@users.noreply.github.com>
2018-11-27 14:58:45 -0800

    20181127-01

--- inc/search-route.php
+++ inc/search-route.php
@@ -10,6 +10,23 @@ function my_theme_register_search() {
 
 add_action('rest_api_init', 'my_theme_register_search');
 
+//* We only need to output array
+//* WordPress will convert array to JSON format
 function my_theme_search_results() {
-  return 'you reached a route';
+  // return ['red', 'yellow', 'blue'];
+  $the_query = new WP_Query([
+    'post_type' => 'professor'
+  ]);
+
+  $rtn = [];
+
+  while ($the_query->have_posts()) {
+    $the_query->the_post();
+    array_push($rtn, [
+      'title' => get_the_title(),
+      'permalink' => get_the_permalink(),
+    ]);
+  }
+
+  return $rtn;
 }
